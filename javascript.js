dd813005e0a3aaa7d712b3bc06393a1ead114517
Brayan <gonzalezbrayan2014@gmail.com>
2025-04-03 16:21:57 -0500

    added getHumanChoice function

--- javascript.js
+++ javascript.js
@@ -2,12 +2,22 @@ let getComputerChoice = () => {
     if(Math.random()>.5){
         return "Rock";
     }else if(Math.random() <.5){
-        return "paper";
+        return "Paper";
     }else{
-        return "scizzors";
+        return "Scizzors";
 
     }
     
 }
 
 
+let getHumanChoice = () => {
+    let choice = prompt("Choose: Rock, Paper, Scizzors?");
+    if("Rock"){
+        return "Rock";
+    }else if("Paper"){
+        return "Paper";
+    }else{
+        return "Scizzors";
+    }
+}
