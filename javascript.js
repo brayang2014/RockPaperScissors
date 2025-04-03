85d82dd13f680219c24005b9e238908e6653a68a
Brayan <gonzalezbrayan2014@gmail.com>
2025-04-03 16:17:08 -0500

    added getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let getComputerChoice = () => {
+    if(Math.random()>.5){
+        return "Rock";
+    }else if(Math.random() <.5){
+        return "paper";
+    }else{
+        return "scizzors";
+
+    }
+    
+}
+
+
